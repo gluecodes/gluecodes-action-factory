a9e02f454a2b6e1ed662e8a6f4d9ff320ff12b82
Krzysztof Czopp <chris.czopp@premote.in>
2018-03-19 13:09:41 +0000

    remove init backend action

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { createAction } = require('./src/create-action');
-const { initBackendActions } = require('./src/init-backend-actions');
 
 module.exports = {
-  createAction,
-  initBackendActions
+  createAction
 };
